710c2e5a03045e660df82d21e1978940e30e601c
pikapikapow <vladimirhwang84@gmail.com>
2023-12-31 11:18:43 -0500

    Content placholder finished

---
+++ script.js
@@ -0,0 +1,27 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="desk_computer.jpg" alt="computer" />'
+
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  name.innerHTML = 'Dave Hill'
+  date.innerHTML = 'Sep 26, 2021'
+
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+
+
+}
